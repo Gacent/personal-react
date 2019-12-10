ae4346c5d8ec114b331972d4a5324e087b530ef4
JasonJohn <794733453@qq.com>
2019-12-10 11:54:46 +0800

    fix: 搭建目录结构

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React ,{Component,Fragment}from 'react';
+import {
+  Test1,
+  Test2
+} from './components'
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Test1></Test1>
+        <Test2></Test2>
+      </Fragment>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// import Test1 from './Test1'
+// import Test2 from './Test2'
+
+// export {
+//   Test1,
+//   Test2
+// }
+
+export { default as Test1 } from './Test1'
+export { default as Test2 } from './Test2'
