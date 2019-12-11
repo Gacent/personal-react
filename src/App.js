c596813d4aa0f9fb177d7c3b20614e360471b8a5
JasonJohn <794733453@qq.com>
2019-12-11 11:22:11 +0800

    feat: 生命周期篇

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React ,{Component,Fragment}from 'react';
 import {
-  Test1,
-  Test2
+  Test1
 } from './components'
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
-        <Test1></Test1>
-        <Test2></Test2>
+        <Test1 title="test1"></Test1>
       </Fragment>
     )
   }
