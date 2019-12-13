a0acc1a9acf22933b07b84cecd4e42e24fbc263b
JasonJohn <794733453@qq.com>
2019-12-13 16:39:19 +0800

    feat: hook

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # personal-react
-## 当没看到
+## react hook
+> 让函数式组件也有state等
+### useState方法
+1. 是一个数组，一个是初始值，一个是赋值的方法。
+```javascript
+const [name,setName]=useState(10)
+// setName(接收一个数值)
+```
+2. 可以有多个
+3. 参数是默认值，一个useState是一个
+
+### useEffect
+组件更新的时候执行，相比于类组件的componentDidMount和componentDidUpdate
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{useState,useEffect}from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Count=()=>{
+  const [name,setName]=useState(10)
+  useEffect(()=>{
+    console.log('更新le ')
+  })
+  return (
+    <div>
+      <button onClick={()=>setName(name-1)}>-</button>
+      <span>{name}</span>
+      <button onClick={()=>setName(name+1)}>+</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<Count />, document.getElementById('root'));
